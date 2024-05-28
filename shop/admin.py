5f2bc63a8c3ba40a476640deb0fedc821f5057cf
user <naoki582003@yahoo.co.jp>
2024-05-28 15:44:44 +0900

    Add static and media directories

--- shop/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Author, Book, Tag, Cart, Order
-
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(Tag)
-admin.site.register(Cart)
-admin.site.register(Order)
