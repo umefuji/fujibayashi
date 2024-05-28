9cf57b6f2d46f337cb7407f324e856967eb266f7
user <naoki582003@yahoo.co.jp>
2024-05-28 15:48:29 +0900

    Add all project files

---
+++ shop/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path,include
+from . import views
+
+app_name = 'shop' 
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('books/', views.book_list, name='book_list'),
+    path('book/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('add/', views.add_book, name='add_book'),
+    path('edit/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('cart/', views.cart_detail, name='cart_detail'),
+    path('cart/remove/<int:book_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('add_to_cart/<int:book_id>/', views.add_to_cart, name='add_to_cart'),
+    path('place_order/', views.place_order, name='place_order'),
+    path('order_history/', views.order_history, name='order_history'),
+    path('profile/', views.profile, name='profile'),
+    path('accounts/', include('allauth.urls')),
+]
